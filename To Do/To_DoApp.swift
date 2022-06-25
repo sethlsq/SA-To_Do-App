b6ff5d40770ad61eab4b408ad6aff4be666e5a80
mudd258 <81851831+mudd258@users.noreply.github.com>
2022-06-25 10:15:24 +0800

    Initial Commit

---
+++ To Do/To_DoApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_DoApp.swift
+//  To Do
+//
+//  Created by Seth Loh on 25/6/22.
+//
+
+import SwiftUI
+
+@main
+struct To_DoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
